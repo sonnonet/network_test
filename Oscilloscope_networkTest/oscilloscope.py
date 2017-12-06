fcedf5b7ccc76dd741e2a486c2f23df8d70e6211
sonnnonet <korea.sonnonet@gmail.com>
2017-12-06 20:53:52 +0900

    newversion_sht

--- Oscilloscope_networkTest/oscilloscope.py
+++ Oscilloscope_networkTest/oscilloscope.py
@@ -6,16 +6,18 @@ from datetime import datetime, timedelta
 import threading
 
 
-AM_OSCILLOSCOPE = 0x93
+AM_OSCILLOSCOPE = 0xA4
 
 class OscilloscopeMsg(tos.Packet):
     def __init__(self, packet = None):
         tos.Packet.__init__(self,
-                            [('version',  'int', 2),
-                             ('interval', 'int', 2),
-                             ('id',       'int', 2),
-                             ('count',    'int', 2),
-                             ('readings', 'blob', None)],
+                            [('srcID',  'int', 2),
+                             ('seqNo', 'int', 4),
+                             ('ch0Temp', 'int', 2),
+                             ('ch0Humi', 'int', 2),
+                             ('ch1Temp', 'int', 2),
+                             ('ch1Humi', 'int', 2),
+                             ],
                             packet)
 if '-h' in sys.argv:
     print "Usage:", sys.argv[0], "serial@/dev/ttyUSB0:57600"
@@ -23,37 +25,21 @@ if '-h' in sys.argv:
 
 am = tos.AM()
 
-startcount=0
-stopcount=0
-init =1
 
-before_now = datetime.now()
-timebreak = before_now + timedelta(seconds=10)
-print "starting time: " , before_now
 while True:
 	p = am.read()
 	if p and p.type == AM_OSCILLOSCOPE:
 	    msg = OscilloscopeMsg(p.data) 
-	    if(init):
-		startcount = msg.count
-		init = 0
        # print msg.id, msg.count, [i<<8 | j for (i,j) in zip(msg.readings[::2], msg.readings[1::2])]
 #	now = datetime.now()
 #	timegap = timedelta(seconds=1)
 #        timeexit = timedelta(seconds=10)
-	msg.count = int(msg.count) + 1000
-	print "id: ", msg.id, " count : ",  msg.count, "moteinterval : ", msg.interval, "version : ", msg.version
-	print before_now
-	after_now = datetime.now()
-	print after_now
-	if(before_now.second + 10 == after_now.second):
-		stopcount = msg.count
-		print "ending time:", timebreak
-		print "........................... report ................................."
-		print " total checking time : start ", before_now 
-		print "                       stop  ", timebreak
-		print " start count : ", startcount
-		print " stop count  : ", stopcount 
-		print " total count : ", stopcount - startcount
-		print " packet per second :", (stopcount - startcount)/10 
-		break;
+        #humi_0 = -6 + 125 *  (msg.ch0Humi / (2^14))
+        humi_0 = -2.0468 + (0.0367*msg.ch0Humi) + (-1.5955*0.000001)*msg.ch0Humi*msg.ch0Humi
+        temp_0 = -(39.6) + (msg.ch0Temp * 0.01)
+        humi_1 = -2.0468 + (0.0367*msg.ch1Humi) + (-1.5955*0.000001)*msg.ch1Humi*msg.ch1Humi
+        temp_1 = -(39.6) + (msg.ch1Temp * 0.01)
+
+	print "id: ", msg.srcID, " count : ",  msg.seqNo, \
+          " Temp_0 : ", temp_0, "Humi_0 : ", humi_0, \
+          " Temp_1 : ", temp_1, "Humi_1 : ", humi_1
